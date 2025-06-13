12a840db5fe09363317f514514cd5c26e5c63e00
sylph611 <ccg0611a@gmail.com>
2025-06-14 00:08:59 +0900

    first commit

---
+++ src/test/java/org/sylph/everything/EverythingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sylph.everything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverythingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
